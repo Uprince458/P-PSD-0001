dae4732ac7d23e29111969402632d54d26943c29
Prince Upadhyay <princessupadhyay97@gmail.com>
2026-02-05 10:15:38 +0000

    Add CS question paper

---
+++ papers/cs.js
@@ -0,0 +1,16 @@
+// Global container for all papers
+window.questionPapers = window.questionPapers || {};
+
+// =======================
+// CS QUESTION PAPER
+// =======================
+window.questionPapers["CS Test"] = {
+  password: "CS123",
+  questions: [
+    {
+      question: "Convert to 8421 to binary",
+      options: ["0010", "0001", "1110", "1111"],
+      answer: "Speed"
+    }
+  ]
+};
