7fd9c6fe8c4517eca34fe8c1fc75bc9b98d69f96
Prince Upadhyay <princessupadhyay97@gmail.com>
2026-02-05 10:11:16 +0000

    Add chemistry question paper

---
+++ papers/chemistry.js
@@ -0,0 +1,16 @@
+// Global container for all papers
+window.questionPapers = window.questionPapers || {};
+
+// =======================
+// CHEMISTRY QUESTION PAPER
+// =======================
+window.questionPapers["Chemistry Test"] = {
+  password: "CHEM123",
+  questions: [
+    {
+      question: "What is atom.",
+      options: ["Unit", "Compound", "Particle", "Matter"],
+      answer: "Particle"
+    }
+  ]
+};
